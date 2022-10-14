9f0b10c8bbb6485a311de9123789579bdcf00dc2
MaximoDiaz1990 <77713582+MaximoDiaz1990@users.noreply.github.com>
2022-10-13 22:42:52 -0500

    Update nodejsDSL.groovy

--- DSL/nodejsDSL.groovy
+++ DSL/nodejsDSL.groovy
@@ -1,9 +1,9 @@
 job('Aplicacion Node.js DSL') {
     description('Aplicación Node JS DSL para el curso de Jenkins')
     scm {
-        git('https://github.com/macloujulian/nodejsapp.git', 'master') { node ->
-            node / gitConfigName('macloujulian')
-            node / gitConfigEmail('macloujulian@gmail.com')
+        git('https://github.com/MaximoDiaz1990/nodejsapp.git', 'master') { node ->
+            node / gitConfigName('MaximoDiaz1990')
+            node / gitConfigEmail('maximo_diaz_1990@hotmail.com')
         }
     }
     triggers {
